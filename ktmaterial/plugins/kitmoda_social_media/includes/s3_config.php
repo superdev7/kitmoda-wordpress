07a1309860fd3d154b14a35f7d063e8e6eaffcbc
SuperDev <superdev7@gmail.com>
2017-01-20 15:06:31 -0500

    update s3 access again

--- ktmaterial/plugins/kitmoda_social_media/includes/s3_config.php
+++ ktmaterial/plugins/kitmoda_social_media/includes/s3_config.php
@@ -6,8 +6,8 @@
 **/
 
 define('S3_BUCKET', 'payload-k1'); //Changed from orkint to kitmoda
-define('S3_ACCESS_KEY_ID', 'AKIAJXFC3FSC4ZBCFEXQ');
-define('S3_SECRET', '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');
+define('S3_ACCESS_KEY_ID', 'AKIAIQBXPUFXEP64KR3Q');
+define('S3_SECRET', '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');
 define('S3_ACL', 'private');
 define('S3_URL', 'https://'.S3_BUCKET.'.s3.amazonaws.com/');
 define('S3_DOWNLOAD_EXPIRE_TIME', '+2 hours');
