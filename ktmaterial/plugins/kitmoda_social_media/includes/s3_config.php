b139e1052803a1336dd6d3816de37b48fd7573e0
SuperDev <superdev7@gmail.com>
2017-01-20 14:52:32 -0500

    updated S3 crendtials

--- ktmaterial/plugins/kitmoda_social_media/includes/s3_config.php
+++ ktmaterial/plugins/kitmoda_social_media/includes/s3_config.php
@@ -5,9 +5,9 @@
  *
 **/
 
-define('S3_BUCKET', 'kitmoda'); //Changed from orkint to kitmoda
-define('S3_ACCESS_KEY_ID', 'AKIAJJRFM43KLNS3CHDA');
-define('S3_SECRET', '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');
+define('S3_BUCKET', 'payload-k1'); //Changed from orkint to kitmoda
+define('S3_ACCESS_KEY_ID', 'AKIAJXFC3FSC4ZBCFEXQ');
+define('S3_SECRET', '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');
 define('S3_ACL', 'private');
 define('S3_URL', 'https://'.S3_BUCKET.'.s3.amazonaws.com/');
 define('S3_DOWNLOAD_EXPIRE_TIME', '+2 hours');
