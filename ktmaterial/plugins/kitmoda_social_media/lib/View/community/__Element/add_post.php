7b3a4e2efab3b3e80547822b43571a4e84a6f158
Steve Melcher <xixao3d@gmail.com>
2017-01-27 01:08:50 -0500

    POST button auth class and click functionality

--- ktmaterial/plugins/kitmoda_social_media/lib/View/community/__Element/add_post.php
+++ ktmaterial/plugins/kitmoda_social_media/lib/View/community/__Element/add_post.php
@@ -31,6 +31,7 @@ $auth_required = get_current_user_id() ? false : true;
                 
 
                         <div class="add_post_container">
+                        <? /*
                         <a href="" <?=($auth_required ? 'atrqt="community_add_post" ' : '')?>class="btn_add_post btn <?=($auth_required ? '' : 'btn_form_smt')?>"></a>
                                         <div class="add_post">
                                         </div>
@@ -38,7 +39,8 @@ $auth_required = get_current_user_id() ? false : true;
                                         </div>                            
                                     
                         </div>
-                        <div class="add_post_text_container">
+                        */ ?>
+                        <div class="add_post_text_container <?=($auth_required ? '' : 'btn_form_smt')?>">
                             <span class="add_post_text">POST</span>
                         </div>
                 
