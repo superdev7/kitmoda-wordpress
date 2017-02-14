535aaca2c9886011ca05c263fd773ccea025b68a
Steve Melcher <xixao3d@gmail.com>
2017-02-14 14:28:26 -0500

    [v2] Sidebar grid sizing

--- ktmaterial/plugins/kitmoda_social_media/lib/View/community/__Element/sidebar.php
+++ ktmaterial/plugins/kitmoda_social_media/lib/View/community/__Element/sidebar.php
@@ -1,13 +1,13 @@
-<div style="width: 260px; float:right;">   
-    <div class="community_sidebar_outline_shadow">  
+<div class="col-md-3 community_sidebar">
+    <div class="community_sidebar_outline_shadow">
     <div class="community_sidebar_inset_shadow">
         <div class="community_sidebar_overlay_radial">
-          
+
             <div class="inner_sidebar" style="padding-left: 20px; padding-right: 20px;">
                <div class="search">
                    <input name="q" id="ff_q" ng-model="fData_q" facet-input-model="fData.q" type="text" placeholder="Search Posts..." value="" />
                    <div ng-click="faset_set_q($event)" class="ksm_community_search_magnify"><div class="ksm_community_search_magnify_glow"></div></div>
-                   
+
                   <div class="clr"></div>
                </div>
                <div class="filter_header">
@@ -20,7 +20,7 @@
                </div>
                <div class="community_sidebar_linebreak_dark"></div>
                <div class="community_sidebar_linebreak"></div>
-               
+
                <div class="field" style="padding-top: 5px; padding-bottom: 2px;">
                   <div class="sidebar_checkbox">
                       <input type="checkbox" icheck-class="icheckbox_futurico" icheck-collection="fData.topic" ng-model="fData_topic_model" id="ff_topic_model" value="model" class="opt_filter" />
@@ -60,9 +60,9 @@
                   <div>
                      <label for="ff_topic_finished" class="sidebar_filtername">Completed Projects</label>
                   </div>
-               </div><?php if($this->KUser->Auth) : ?>				
-                   
-               
+               </div><?php if($this->KUser->Auth) : ?>
+
+
                <div class="field" style="">
                   <div class="sidebar_checkbox">
                      <input icheck="icheckbox_futurico" type="checkbox" ng-model="fData.following" id="ff_following" ng-true-value="'following'" class="opt_filter" />
@@ -71,9 +71,9 @@
                      <label for="ff_following" class="sidebar_filtername">Following</label>
                   </div>
                </div><?php endif; ?>
-               
-               
-               
+
+
+
                <div class="field" style="">
                   <div class="sidebar_checkbox">
                       <input icheck="icheckbox_futurico" type="checkbox" name="with_images" ng-model="fData.with_images" ng-true-value="'with_images'" id="ff_with_images" class="opt_filter" />
@@ -81,13 +81,13 @@
                   <div>
                      <label for="ff_with_images" class="sidebar_filtername">With Images</label>
                   </div>
-               </div>	
-               
-               
+               </div>
+
+
                <?php if($this->KUser->Auth) : ?>
                <div class="community_sidebar_linebreak_dark"></div>
                <div class="community_sidebar_linebreak"></div>
-               
+
                <div class="field" style="">
                   <div class="sidebar_checkbox">
                       <input icheck="icheckbox_futurico" type="checkbox" name="my_posts" ng-model="fData.my_posts" ng-true-value="'my_posts'" id="ff_my_posts" class="opt_filter" />
@@ -95,21 +95,21 @@
                   <div>
                      <label for="ff_my_posts" class="sidebar_filtername">Show only my posts</label>
                   </div>
-               </div>	
-               
+               </div>
+
                <?php endif; ?>
-               
+
                <div class="community_sidebar_linebreak_dark"></div>
                <div class="community_sidebar_linebreak"></div>
-               
+
                <div class="sort_header" style="">
                   <div class="heading" style="font-size: 13px; font-family: Montserrat;padding-top: 10px;">SORT BY</div>
                   <div class="clr"></div>
-               </div>				
-               
+               </div>
+
                <div class="community_sidebar_linebreak_dark"></div>
                <div class="community_sidebar_linebreak"></div>
-               
+
                <div class="field">
                    <select ng-model="fData.sort" id="ff_sort" class="dropdown_select_test">
                           <option value="">Newest</option>
@@ -119,8 +119,8 @@
                </div>
                <input type="hidden" ng-model="fData.page" value="" id="ff_page" class="opt_filter" />
             </div>
-            
- 
+
+
             </div>
         </div>
     </div>
