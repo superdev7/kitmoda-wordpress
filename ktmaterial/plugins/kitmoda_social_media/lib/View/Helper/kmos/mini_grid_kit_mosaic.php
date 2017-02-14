7033a4a4fc6c657ef35b436260eff61581403818
Steve Melcher <xixao3d@gmail.com>
2017-02-14 13:16:00 -0500

    [v2] Mosaic troubleshooting

--- ktmaterial/plugins/kitmoda_social_media/lib/View/Helper/kmos/mini_grid_kit_mosaic.php
+++ ktmaterial/plugins/kitmoda_social_media/lib/View/Helper/kmos/mini_grid_kit_mosaic.php
@@ -1,5 +1,5 @@
 <div class="mini_grid_view_container">
-    
+
     <?php /*
     <div class="bg_container">
         <div class="bg">
@@ -8,23 +8,23 @@
         </div>
     </div>
     */ ?>
-    
+
     <?php /*
     <div class="loading">
         <div class="goverlay"></div>
         <?php include KSM_VIEWS_PATH.'__Element/loading.php'; ?>
     </div>
     */ ?>
-    
+
     <div class="grid_view_kit_mosaic">
         <div class="grid_kit_mosaic">
-        <?php 
-        //$item_index = 0;
-        //foreach($this->results as $p) : 
-        //    if($this->count_mini_grid_view) KSM_postView::add($p);
-        //    echo $this->renderGridItem($p, $item_index);
-        //    $item_index++;
-        //endforeach;
+        <?php
+        $item_index = 0;
+        foreach($this->results as $p) :
+           if($this->count_mini_grid_view) KSM_postView::add($p);
+           echo $this->renderGridItem($p, $item_index);
+           $item_index++;
+        endforeach;
         ?>
         </div>
     </div>
