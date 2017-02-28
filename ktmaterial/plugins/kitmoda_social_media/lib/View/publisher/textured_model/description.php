bbb45d51566a02f0503e818bd08d172bd67abeb7
SuperDev <superdev7@gmail.com>
2017-02-28 12:56:18 -0500

    Changed all publisher category dropdown lists to DESC instead of ASC

--- ktmaterial/plugins/kitmoda_social_media/lib/View/publisher/textured_model/description.php
+++ ktmaterial/plugins/kitmoda_social_media/lib/View/publisher/textured_model/description.php
@@ -52,7 +52,7 @@
             <div class="field_group">
                 
                 <div class="kcat">
-                    <?=KSM_Taxonomy::dropdown(array('label'=>'Main Category', 'orderby' => 'name', 'order' => 'ASC'));?>
+                    <?=KSM_Taxonomy::dropdown(array('label'=>'Main Category', 'orderby' => 'name', 'order' => 'DESC'));?>
                 </div>
                 
                 
